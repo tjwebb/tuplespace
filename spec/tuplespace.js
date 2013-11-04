a9db7adf0b7993b0b902a0c86b0036f35f096268
tjwebb <travis@traviswebb.com>
2013-11-04 01:02:45 -0500

    initial commit

---
+++ spec/tuplespace.js
@@ -0,0 +1,11 @@
+var T = require('../t.js'),
+  assert = require('chai').assert;
+
+describe('T.space', function () {
+  describe('sanity', function () {
+    it('exists', function () {
+      assert.ok(T);
+      assert.ok(T.space);
+    });
+  });
+});
