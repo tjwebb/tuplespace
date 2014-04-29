d1857360db4a153e62faa360bba37444ef6893ef
Travis Webb <me@traviswebb.com>
2014-04-29 06:53:20 +0000

    added tests for #attributes

--- spec/tuplespace.js
+++ spec/tuplespace.js
@@ -5,26 +5,44 @@ var assert = require('assert'),
 describe('tuplespace', function () {
   var Tuple = space.Tuple;
 
-  describe('#select', function () {
-    before(function () {
-      Tuple.create({ foo: 'bar' });
-    });
-    it('original implementation should still work to spec', function () {
-      var found = Tuple.select(function (record) {
-        return record.foo === 'bar';
+  describe('Tuple', function () {
+
+    describe('#attributes', function () {
+      before(function () {
+        Tuple.create({ test: 'attributes', name: 'tjw' });
       });
-      assert.equal(found.length, 1);
-      assert.equal(found[0].foo, 'bar');
+      it('should return all attributes originally given via #create', function () {
+        var results = Tuple.select({ test: 'attributes' });
+        assert.equal(_.first(results).name, 'tjw');
+        assert(_.has(_.first(results).attributes(), 'test'));
+        assert(_.has(_.first(results).attributes(), 'name'));
+      });
+      it('should filter out functions specified as model properties', function () {
+        Tuple.create({ test: 'attributes', name: 'tjw', pointless: function () { return 1; } });
+        var results = Tuple.select({ test: 'attributes' });
+        assert(!_.has(_.first(results).attributes(), 'pointless'));
+      });
     });
-    it('should find a simple tuple with exact template', function () {
-      var found = Tuple.select({ foo: 'bar' });
-      assert.equal(found.length, 1);
-      assert.equal(found[0].foo, 'bar');
+
+    describe('#select', function () {
+      before(function () {
+        Tuple.create({ test: 'select', name: 'tjw' });
+      });
+      it('original implementation should still work to spec', function () {
+        var results = Tuple.select(function (record) {
+          return record.test === 'select';
+        });
+        assert.equal(_.first(results).name, 'tjw');
+      });
+      it('should find a simple tuple with exact template', function () {
+        var results = Tuple.select({ test: 'select' });
+        assert.equal(_.first(results).name, 'tjw');
+      });
+      it('should find a simple tuple with similar template', function () {
+        var results = Tuple.select({ test: /sele/ });
+        assert.equal(_.first(results).name, 'tjw');
+      });
     });
-    it('should find a simple tuple with similar template', function () {
-      var found = Tuple.select({ foo: _.isString });
-      assert.equal(found.length, 1);
-      assert.equal(found[0].foo, 'bar');
-    });
+
   });
 });
